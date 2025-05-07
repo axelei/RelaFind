5aad359a53a942eea67124f6293c2fa3d7a23f72
Krusher <axelei@gmail.com>
2025-05-07 19:11:18 +0200

    Flags

--- main.c
+++ main.c
@@ -1,24 +1,54 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "main.h"
+#include <stdbool.h>
 
 #define SHIFTS 0xff
+#define TITLE "RelaFind by Krusher - Licensed under GPLv3\n"
 
 static unsigned char* haystack;
+static bool silent = false;
+static bool count = false;
 
 int main(const int argc, const char *argv[]) {
 
-    printf("RelaFind by Krusher - Licensed under GPLv3\n");
 
     if (argc < 3) {
-        printf("Usage: %s <file> <needle>\n", argv[0]);
+        printf(TITLE);
+        printf("Usage: %s <file> <needle> [flags]\n", argv[0]);
+        printf("Flags go all in the same word and can be:\n");
+        printf("s: Silent mode: display only occurrences.\n");
+        printf("c: Count mode: display only the number of occurrences.\n");
         return 1;
     }
 
+    const char* filename = argv[1];
     const char* needle = argv[2];
+    if (argc > 3) {
+        const char* flags = argv[3];
+        const size_t flags_len = strlen(flags);
+        for (int i = 0; i < flags_len; i++) {
+            switch (flags[i]) {
+                case 's':
+                    silent = true;
+                break;
+                case 'c':
+                    count = true;
+                break;
+                default:
+                    printf("Unknown flag: %c\n", flags[i]);
+                    return 1;
+            }
+        }
+    }
+
+    if (!silent && !count) {
+        printf(TITLE);
+    }
+
     long size = 0;
-    unsigned char* file = read_file(argv[1], &size);
+    unsigned char* file = read_file(filename, &size);
 
     if (needle[0] == '\0') {
         printf("Empty needle\n");
@@ -30,7 +60,9 @@ int main(const int argc, const char *argv[]) {
     }
 
 
-    printf("File: %s, size: %ld, needle: %s\n", argv[1], size, needle);
+    if (!silent && !count) {
+        printf("File: %s, size: %ld, needle: %s\n", filename, size, needle);
+    }
 
     const size_t needle_len = strlen(needle);
     haystack = malloc(needle_len);
@@ -40,14 +72,18 @@ int main(const int argc, const char *argv[]) {
         const int offset = detect(file, i, needle);
         if (offset != -1) {
             coincidences++;
-            printf("Found at position: %ld (0x%lx) Offset was: %d\n", i, i, offset);
+            if (!count) {
+                printf("\"%s\" Found at \"%s\", position: %ld (0x%lx) Offset was: %d\n", needle, filename, i, i, offset);
+            }
         }
     }
 
     free(haystack);
     free(file);
 
-    printf("Coincidences: %d\n", coincidences);
+    if (!silent) {
+        printf("Coincidences: %d\n", coincidences);
+    }
 
     return 0;
 }
